a424a1ef43e2eaefcc0e7393fa71346918cc3ff6
masum-khondhoker-efaz <efazkh@gmail.com>
2025-11-03 12:48:36 +0600

    app.ts file modified

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,36 @@
-import cors from "cors";
-import express, { Application, NextFunction, Request, Response } from "express";
-import httpStatus from "http-status";
-import globalErrorHandler from "./app/middlewares/globalErrorHandler";
-import router from "./app/routes";
-import { logger, loggerConsole } from "./app/middlewares/logger";
-import path from "path";
-import bodyParser from "body-parser";
-import { PaymentController } from "./app/modules/payment/payment.controller";
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/routes';
+import { logger, loggerConsole } from './app/middlewares/logger';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { PaymentController } from './app/modules/payment/payment.controller';
 
 const app: Application = express();
 
 app.use(logger);
 app.use(loggerConsole);
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3001",
-//       "http://localhost:3000",
-//       "http://10.10.20.60:3005",
-//       "http://10.10.20.60:3006",
-//       "http://10.10.20.60:3007",
-//       "http://10.10.20.60:3008",
-      
-
-//     ],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3001',
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'http://13.62.189.94:5173',
+      'http://13.62.189.94:5174',
+      'http://13.62.189.94:3000',
+      'http://10.10.20.60:3000',
+      'https://ecommerce-website-rho-lemon.vercel.app',
+      'https://hatem-dashboard.vercel.app',
+    ],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  }),
+);
 // app.use(
 //   cors({
 //     origin: true,
@@ -35,15 +38,15 @@ app.use(loggerConsole);
 //     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 //   }),
 // );
-app.use(
-  cors(
-    {
-    origin: "*", // Allow all origins for development
-    credentials: true, // Allow credentials
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  }
-)
-);
+// app.use(
+//   cors(
+//     {
+//     origin: "*", // Allow all origins for development
+//     credentials: true, // Allow credentials
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   }
+// )
+// );
 
 app.use(
   '/api/v1/stripe/payment-webhook',
@@ -55,27 +58,25 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send({
-    Message: "SpareDoc server is now live. . .",
+    Message: 'SpareDoc server is now live. . .',
   });
 });
 // Serve static files from 'public' folder
-app.use(express.static(path.join(process.cwd(), "public")));
+app.use(express.static(path.join(process.cwd(), 'public')));
 
-app.use("/api/v1", router);
+app.use('/api/v1', router);
 
 app.use(globalErrorHandler);
-
-
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.status(httpStatus.NOT_FOUND).json({
     success: false,
-    message: "API NOT FOUND!",
+    message: 'API NOT FOUND!',
     error: {
       path: req.originalUrl,
-      message: "Your requested path is not found!",
+      message: 'Your requested path is not found!',
     },
   });
 });
